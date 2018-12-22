c4f0c89236d016a988927fe141291750a3ceb5ea
csenge <csengeweb@gmail.com>
2018-12-22 16:58:40 +0100

    added data from App.js; updated filterLocals BUG

--- src/SidebarContainer.js
+++ src/SidebarContainer.js
@@ -8,41 +8,85 @@ class SidebarContainer extends Component {
         super(props); 
             this.state = {
                 query:'',
-                shownMarkers: [],
-                singleMarker: []
+                listOfLokalElements: '',
+                newLocals:''
+                //shownMarkers: [],
+                //singleMarker: []
             };
 
-        //this.filterMarkers = this.filterMarkers.bind(this);
+        //this.filterLokals = this.filterLokals.bind(this);
     
     }
-    
+
+   listOfSelectedLocals = [];
+   /*
     updateQuery = (query) => {
         this.setState({query: query})
     }
-
-    /*clearQuery = () => {
+    
+    clearQuery = () => {
         this.setState({ query: '' })
     }
     */
-   
-    /*/Update markers on the map according search bar input 
-   filterMarkers =() =>{
-    if (this.state.query) {
-        this.props.locals.filter((singleMarker) =>
-            test(singleMarker.name));
-        singleMarker.marker.setVisible(true);
-        this.state.shownMarkers.push(singleMarker);                  
-    } else {
-        singleMarker.marker.setVisible(false);
-    };
-    this.setState({
-        'shownMarkers' : this.state.shownMarkers,
-        'query' : this.state.query
-        })
+  
+   filterLocals  = () => {
+        let listOfLokalElements = [];
+       
+        //console.log(document.getElementById('search_box').value);
+        //console.log('this.state.query értéke:' + this.state.query);
+        let userValue = document.getElementById('search_box').value;
+        var new_locals = [];
+
+        this.props.locals.forEach(function(local) {
+            //console.log('megadott érték:'+userValue);
+            //console.log('keresett érték:'+local.name);
+            if (local.name === userValue){
+               new_locals.push(local);
+               console.log('talalat');
+            }
+        }); 
+        this.setState({newLocals:new_locals}); 
+        console.log('new_locals értéke:' + new_locals);
+
+        /*this.setState({query: userValue});
+    //this.props.locals.forEach(function (LokalElement) {
+            if (this.state.query) {
+               // console.log('Query tartalma' + this.state.query)
+                //const match = new RegExp(escapeRegExp(this.state.query), 'i');
+                //console.log('match értéke:' + match);
+                console.log('props.local értéke' + this.props.locals);
+                 /*test = this.props.locals.filter(function test(local) {
+                   if (local === userValue){
+                       return 'meg van';
+                   }
+                   else {
+                       return 'nincs';
+                   }
+                    
+                    //match.test(local.name));               
+                    listOfLokalElements.push(listOfLokalElements)
+                 } ); 
+               
+                
+                //this.local.marker.setVisible(true);
+                //search for Foursq_api(id)
+            } else {
+                listOfLokalElements = this.props.locals;
+
+                //this.local.marker.setVisible(false);
+            };
+         // });
+
+        this.setState({
+            listOfLokalElements: listOfLokalElements,
+            //query : query
+        });
+        */ 
     }
-    */
+  
+
     render() {
-        console.log('Props', this.props)
+        /*console.log('Props', this.props)
 
         let showingListOfLocals;
         if (this.state.query) {
@@ -52,8 +96,9 @@ class SidebarContainer extends Component {
                 match.test(local.name));               
             } else {
                 showingListOfLocals = this.props.locals;
-            } 
-        
+            };
+        //Passing showingListOfLocals array to App.js
+        */
        
 
         /*/https://developers.google.com/maps/documentation/javascript/examples/marker-remove         
@@ -104,8 +149,10 @@ class SidebarContainer extends Component {
                     <input 
                         className="input-field" 
                         type="text" 
-                        value={this.state.query} 
-                        onChange={ event => this.updateQuery(event.target.value) && this.filterMarkers}  
+                        id="search_box"
+                        /*value={this.state.query} */
+                        onChange={ this.filterLocals}
+                        //onChange={event => this.updateQuery(event.target.value) }   
                         placeholder="Search for café..." 
                     />
                     <button
@@ -116,9 +163,10 @@ class SidebarContainer extends Component {
                     </button>
                 </form>     
                 <ul className="Sidebar-list">
-                    {showingListOfLocals.map((listElement) => (
-                    <li className= "li" key={listElement.foursquareId}>
+                    {this.state.newLocals.map((listElement) => (
+                    <li className= "li" key={listElement.foursquareId} selectedLocal = {this.props.selectedLocalVenue} onClick = {this.props.select}>
                         {listElement.name}
+                        
                     </li>))
                     }
                 </ul>
