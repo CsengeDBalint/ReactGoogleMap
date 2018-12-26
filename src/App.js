eda42ac3f829b21113b04e71169140dcab28151b
csenge <csengeweb@gmail.com>
2018-12-26 16:44:22 +0100

    deleted locals from App.js

--- src/App.js
+++ src/App.js
@@ -13,81 +13,9 @@ class App extends Component {
     this.state = {
       allMarkers: [],
       infoWindows : [],
-      selectedLocalVenue:'',
+      selectedLocalVenue:null,
       //cafeLocals:[],
-      error: null,
-      /*locals: [
-        {
-            foursquareId: "4b058890f964a520afcd22e3",
-            name:"Café Central",
-            position: { lat: 48.21034849634383, lng: 16.36539101600647},
-            address: "Herrengasse 14 (Strauchg.)",
-            postalcode: "1010 Vienna"
-        },
-        {
-            foursquareId: "4b058890f964a520a8cd22e3",
-            name:"Café Landtmann",
-            position: { lat: 48.21150494287856, lng: 16.36135697364807},
-            address: "Universitätsring 4",
-            postalcode: "1010 Vienna"
-        },
-        {
-            foursquareId: "4af9b000f964a520561322e3",
-            name:"phil",
-            position: { lat: 48.200196163700625, lng: 16.36121911866486},
-            address: "Gumpendorfer Str. 10-12",
-            postalcode: "1060 Vienna"
-        },
-        {
-            foursquareId: "4b058890f964a520a9cd22e3",
-            name:"Café Mozart",
-            position: { lat: 48.2043173992853, lng: 16.368986707234214},
-            address: "Albertinaplatz 2",
-            postalcode: "1010 Vienna"
-        },
-        {
-            foursquareId: "4b064b09f964a5206dea22e3",
-            name:"Café Sacher",
-            position: { lat: 48.203857, lng: 16.370144},
-            address: "Philharmonikerstr. 4",
-            postalcode: "1010 Vienna"
-        },
-        {
-            foursquareId: "4c39dab018e72d7ffccd1af5",
-            name:"Motto am Fluss",
-            position: { lat: 48.2122554233485, lng: 16.377926837416943},
-            address: "Schwedenplatz 2",
-            postalcode: "1010 Vienna"
-        },
-        {
-            foursquareId: "4b058890f964a520b1cd22e3",
-            name:"Demel – K.u.K. Hofzuckerbäcker",
-            position: { lat: 48.20849018101388, lng: 16.367204970966302},
-            address: "Kohlmarkt 14",
-            postalcode: "1010 Vienna"
-        },
-        {
-            foursquareId: "4b058890f964a520adcd22e3",
-            name:"Café Sperl",
-            position: { lat: 48.199844, lng: 16.361171},
-            address: "Gumpendorfer Str. 11",
-            postalcode: "1010 Vienna"
-        },
-        {
-            foursquareId: "4b9e3a64f964a52063d336e3",
-            name:"Le Bol",
-            position: { lat: 48.206469, lng: 16.370575},
-            address: "Neuer Markt 14",
-            postalcode: "1010 Vienna"
-        },
-        {
-            foursquareId: "4b1a96a5f964a520ebec23e3",
-            name:"Kleines Café",
-            position: { lat: 48.20647558079986, lng: 16.374263927297903},
-            address: "Franziskanerplatz 3",
-            postalcode: "1010 Vienna"
-        }
-    ]*/
+      error: null
         };
         
         //this.initMap = this.initMap.bind(this);
@@ -126,6 +54,9 @@ class App extends Component {
         });
         //TODO: output the venue details in the ListItemDetails
         //TODO: improve error handling See: The Road to learn React page 110
+
+
+
     if(venue) {
         fetch(`https://api.foursquare.com/v2/venues/${venue_id}?client_id=${foursquare_client_id}&client_secret=${foursquare_client_secret}&v=${foursquare_version}`, { signal })
         //fetch(`https://api.foursquare.com/v2/venues/explore?ll=48.208418,16.373231&section=coffee&client_id=${foursquare_client_id}&client_secret=${foursquare_client_secret}&v=${foursquare_version}`, { signal })
