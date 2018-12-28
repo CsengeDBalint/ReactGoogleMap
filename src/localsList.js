8321c86f7f9fa4552d602ce86f6d88047a85e07b
csenge <csengeweb@gmail.com>
2018-12-28 16:11:11 +0100

    Updated localsList

--- src/localsList.js
+++ src/localsList.js
@@ -1,44 +1,44 @@
 export const locals = [
         {
-            foursquareId: "4b058890f964a520afcd22e3",
-            name:"Café Central",
-            position: { lat: 48.21034849634383, lng: 16.36539101600647},
-            address: "Herrengasse 14 (Strauchg.)",
+            foursquareId: "4b1a96a5f964a520ebec23e3",
+            name:"Kleines Café",
+            position: { lat:  48.20647558079986, lng: 16.374263927297903},
+            address: "Franziskanerplatz 3",
             postalcode: "1010 Vienna"
         },
         {
-            foursquareId: "4b058890f964a520a8cd22e3",
-            name:"Café Landtmann",
-            position: { lat: 48.21150494287856, lng: 16.36135697364807},
-            address: "Universitätsring 4",
+            foursquareId: "4b058890f964a520aecd22e3",
+            name:"Café Hawelka",
+            position: { lat: 48.2078439788914, lng: 16.369849144269097},
+            address: "Dorotheergasse 6",
             postalcode: "1010 Vienna"
         },
         {
-            foursquareId: "4af9b000f964a520561322e3",
-            name:"phil",
-            position: { lat: 48.200196163700625, lng: 16.36121911866486},
-            address: "Gumpendorfer Str. 10-12",
+            foursquareId: "4b9e3a64f964a52063d336e3",
+            name:"Le Bol",
+            position: { lat: 48.206469, lng: 16.370575},
+            address: "Neuer Markt 14",
             postalcode: "1060 Vienna"
         },
         {
-            foursquareId: "4b058890f964a520a9cd22e3",
-            name:"Café Mozart",
-            position: { lat: 48.2043173992853, lng: 16.368986707234214},
-            address: "Albertinaplatz 2",
+            foursquareId: "4b6ee368f964a52025cf2ce3",
+            name:"Cafe Korb",
+            position: { lat: 48.21018394120712, lng: 16.370699672828422},
+            address: "Tuchlauben 10",
             postalcode: "1010 Vienna"
         },
         {
-            foursquareId: "4b064b09f964a5206dea22e3",
-            name:"Café Sacher",
-            position: { lat: 48.203857, lng: 16.370144},
-            address: "Philharmonikerstr. 4",
+            foursquareId: "4c8b70a22e33370404d5cc41",
+            name:"L. Heiner Hofzuckerbäcker",
+            position: { lat: 48.20620504430728, lng: 16.371451283958375},
+            address: "Kärntnerstr. 21-23",
             postalcode: "1010 Vienna"
         },
         {
-            foursquareId: "4c39dab018e72d7ffccd1af5",
-            name:"Motto am Fluss",
-            position: { lat: 48.2122554233485, lng: 16.377926837416943},
-            address: "Schwedenplatz 2",
+            foursquareId: "4b058890f964a520a7cd22e3",
+            name:"Café-Restaurant Diglas",
+            position: { lat: 48.208743063975184, lng: 16.375210045601943},
+            address: "Wollzeile 10",
             postalcode: "1010 Vienna"
         },
         {
@@ -49,24 +49,25 @@ export const locals = [
             postalcode: "1010 Vienna"
         },
         {
-            foursquareId: "4b058890f964a520adcd22e3",
-            name:"Café Sperl",
-            position: { lat: 48.199844, lng: 16.361171},
-            address: "Gumpendorfer Str. 11",
+            foursquareId: "4b058890f964a520cacd22e3",
+            name:"Zanoni & Zanoni",
+            position: { lat: 48.21013726877021, lng: 16.37430531024073},
+            address: "Lugeck 7",
             postalcode: "1010 Vienna"
         },
         {
-            foursquareId: "4b9e3a64f964a52063d336e3",
-            name:"Le Bol",
-            position: { lat: 48.206469, lng: 16.370575},
-            address: "Neuer Markt 14",
+            foursquareId: "4b6c1e0ef964a520b6242ce3",
+            name:"Haas & Haas Teehaus",
+            position: { lat: 48.208177561049744, lng: 16.373780800734853
+            },
+            address: "Stephansplatz 4",
             postalcode: "1010 Vienna"
         },
         {
-            foursquareId: "4b1a96a5f964a520ebec23e3",
-            name:"Kleines Café",
-            position: { lat: 48.20647558079986, lng: 16.374263927297903},
-            address: "Franziskanerplatz 3",
+            foursquareId: "4b71b73df964a52026582de3",
+            name:"Café Daniel Mose",
+            position: { lat: 48.21078, lng: 16.374782},
+            address: "Rotenturmstrasse 14",
             postalcode: "1010 Vienna"
         }
     ]
